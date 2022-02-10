e3452493a21adf2d2b508c844ef288d542905f8f
jeremiah <jeresmith@live.com>
2022-02-09 21:00:25 -0600

    new code

--- constants/Colors.js
+++
@@ -1,7 +0,0 @@
-export default {
-    primary: '#FFA500',
-    lightPrimary: '#FFA500',
-    accent: '#F2D2BD',
-    lightAccent: '#FBCEB1',
-    brightBlue: '#FFA500'
-}
--- env.js
+++
@@ -1,6 +0,0 @@
-const vars = {
-    apiUrl: "https://socialapp-serverr.herokuapp.com",
-    defaultImageUri: "https://socialapp-serverr.herokuapp.com/static/images/defaultprofile.jpg"
-};
-
-export default vars;
