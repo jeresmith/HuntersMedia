bb0880d007803ad6dc432706a0c06c1320f8eee6
T-Bunz20 <56098144+T-Bunz20@users.noreply.github.com>
2022-03-03 19:16:33 -0800

    Update Login.js

--- code/components/auth/Login.js
+++ code/components/auth/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Button, TextInput} from 'react-native';
+import { View, Button, TextInput, StyleSheet, Image, Text} from 'react-native';
 
 import firebase from 'firebase/compat';
 
@@ -27,23 +27,56 @@ export class Login extends Component {
     }
   render() {
     return (
-      <View>
-          <TextInput
+      <View style={{paddingTop: 50, backgroundColor: '#90EE90'}}>
+          <Text style = {styles.title}>Hunting Grounds</Text>
+          <Image
+          style = {styles.image}
+          source={require("C:/Users/OWNER/Documents/GitHub/HuntersMedia/code/assetsadaptive-icon.png")}>
+          </Image>
+          <View style = {styles.inputBox}>
+            <TextInput
             placeholder="email"
             onChangeText={(email) => this.setState( {email})}
             />
+          </View>
+          <View style = {styles.inputBox}>
             <TextInput
             placeholder="password"
             secureTextEntry = {true}
             onChangeText={(password) => this.setState( {password})}
             />
-            <Button
-                onPress={() => this.onSignUp()}
-                title = "Sign In"
-            />
+          </View>
+          <View style={{padding: 20}}></View>
+          <Button
+          onPress={() => this.onSignUp()}
+          title = "Sign In"
+          />
+          <View style={{backgroundColor: '#90EE90', padding: 100 }}></View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+    aspectRatio: 1/1,
+    alignSelf: 'center',
+    marginBottom: 50,
+    height: 150,
+    width: 150
+  },
+  title: {
+    textAlign: 'center',
+    marginBottom: 30,
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  inputBox: {
+    borderColor: '#0f0f0f',
+    borderWidth: 1,
+    margin: 10,
+    padding: 10
+  }
+})
 
 export default Login
