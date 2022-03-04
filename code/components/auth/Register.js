d1c83ee8eda571ae95502953d7ef6e91b1214b9f
T-Bunz20 <56098144+T-Bunz20@users.noreply.github.com>
2022-03-03 18:47:08 -0800

    Update Register.js

--- code/components/auth/Register.js
+++ code/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { View, Button, TextInput} from 'react-native';
+import { View, Button, TextInput, StyleSheet, Image, Text} from 'react-native';
 
 import firebase from 'firebase/compat';
 
@@ -32,28 +32,62 @@ export class Register extends Component {
     }
   render() {
     return (
-      <View>
+      <View style = {{backgroundColor: '#90EE90', paddingTop: 50}}>
+        <Text style = {styles.title}>Hunting Grounds</Text>
+        <Image
+        style = {styles.image}
+        source={require("C:\Users\OWNER\Documents\GitHub\HuntersMedia\code\assets\adaptive-icon.png")}>
+        </Image>
+        <View style={styles.inputBox}>
           <TextInput
-            placeholder="username"
-            onChangeText={(username) => this.setState( {username})}
+          placeholder="username"
+          onChangeText={(username) => this.setState( {username})}
           />
-            <TextInput
-            placeholder="email"
-            onChangeText={(email) => this.setState( {email})}
+        </View>
+        <View style={styles.inputBox}>
+          <TextInput
+          placeholder="email"
+          onChangeText={(email) => this.setState( {email})}
           />
-            <TextInput
-            placeholder="password"
-            secureTextEntry = {true}
-            onChangeText={(password) => this.setState( {password})}
+        </View>
+        <View style={styles.inputBox}>
+        <TextInput
+          placeholder="password"
+          secureTextEntry = {true}
+          onChangeText={(password) => this.setState( {password})}
           />
-
-          <Button
-              onPress={() => this.onSignUp()}
-              title = "Sign Up"
-          />
+        </View>
+        <View style={{padding: 20}}></View>
+        <Button
+        onPress={() => this.onSignUp()}
+        title = "Sign Up"
+        />
+        <View style={{backgroundColor: '#90EE90', padding: 100 }}></View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+    aspectRatio: 1/1,
+    alignSelf: 'center',
+    marginBottom: 50,
+    height: 150,
+    width: 150
+  },
+  title: {
+    textAlign: 'center',
+    marginBottom: 30,
+    fontSize: 30,
+    fontWeight: 'bold'
+  },
+  inputBox: {
+    borderColor: '#0f0f0f',
+    borderWidth: 1,
+    margin: 10,
+    padding: 10
+  }
+})
 
 export default Register
