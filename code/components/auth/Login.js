ffcc780378f3ccd4368bdbcf64356604225c9a02
villafanam <57918203+villafanam@users.noreply.github.com>
2022-03-03 19:23:56 -0800

    changed image source path
    changed image source path for image containers
    image source path is now "../../assets/adaptive-icon.png"

--- code/components/auth/Login.js
+++ code/components/auth/Login.js
@@ -31,7 +31,7 @@ export class Login extends Component {
           <Text style = {styles.title}>Hunting Grounds</Text>
           <Image
           style = {styles.image}
-          source={require("C:/Users/OWNER/Documents/GitHub/HuntersMedia/code/assets/adaptive-icon.png")}>
+          source={require("../../assets/adaptive-icon.png")}>
           </Image>
           <View style = {styles.inputBox}>
             <TextInput
